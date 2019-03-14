aa2ab2c1459c7f5f8ab9e96baa1141a8639c4484
daisuke <dsaito85a@gmail.com>
2019-03-14 15:50:02 +0900

    画像ファイルをアップロードする

--- redoru/app/Http/Controllers/DressController.php
+++ redoru/app/Http/Controllers/DressController.php
@@ -8,33 +8,33 @@ use Illuminate\support\Facades\DB;
 
 class DressController extends Controller
 {
-    public function index(Request $request){
+    public function index(Request $request)
+    {
         $items = Dress::all();
-        return view('dress.index' ,['items' => $items]);
+        return view('dress.index', ['items' => $items]);
     }
 
-    public function add(Request $request){
+    public function add(Request $request)
+    {
         return view('dress.add');
     }
 
-    public function create(Request $request){
+    public function create(Request $request)
+    {
 
-        // $this->validate($request, Dress::$rules);
 
-        // $dress = new Dress;
-        // $form = $request->all();
-        // unset($form['_token']);
-        // $dress->fill($form)->save();
+        $filename = $request->image->store('public/dresses');
 
         $param = [
             'id'  => $request->id,
-            'image' => $request->image,
+            'image' => $filename,
             'size' => $request->size,
             'owner_id' => $request->owner_id,
-            'exchange_state' => $request->exchange_state
+            'exchange_state' => $request->exchange_state,
         ];
         DB::table('dresses')->insert($param);
         return redirect('/dress');
+
     }
 
 }
--- redoru/resources/views/dress/add.blade.php
+++ redoru/resources/views/dress/add.blade.php
@@ -8,12 +8,13 @@
 @endsection
 
 @section('content')
+
     <table>
-        <form action="/dress/add" method="post">
+        <form action="/dress/add" method="post" enctype="multipart/form-data">
         {{ csrf_field() }}
         <tr><th>衣装ID</th><td><input type="number" name="id"></td></tr>
         <tr><th>衣装所持者ID</th><td><input type="number" name="owner_id"></td></tr>
-        <tr><th>画像ファイル名</th><td><input type="text" name="image"></td></tr>
+        <tr><th>画像ファイル名</th><td><input type="file" name="image"></td></tr>
         <tr><th>サイズ</th><td><input type="text" name="size"></td></tr>
         <tr><th>交換ステータス</th><td><input type="text" name="exchange_state"></td></tr>
         <tr><th></th><td><input type="submit" value="send"></td></tr>
